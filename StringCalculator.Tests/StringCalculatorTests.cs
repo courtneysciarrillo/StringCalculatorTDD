8b254735df92a2adf4add44a0ea2c7ff945e0801
courtneysciarrillo <courtney.sciarrillo@gmail.com>
2018-07-06 15:15:06 -0400

    first commit

---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        StringCalculator calc;
+
+        [SetUp]
+        public void SetUp()
+        {
+            calc = new StringCalculator();
+        }
+
+        [TestCase("", 0)]
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("1,2", 3)]
+        [TestCase("2,2", 4)]
+        [TestCase("3,3,3", 9)]
+        public void Add_Returns_Expected_Value(string input, int expected)
+        {
+            var result = calc.Add(input);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+
+
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,30 @@
+﻿namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string input)
+        {
+            if (input == string.Empty)
+                return 0;
+
+            if (input.Contains(","))
+            {
+                return CommaSeparatedValuesSum(input);
+            }
+
+            return int.Parse(input);
+        }
+
+        private static int CommaSeparatedValuesSum(string input)
+        {
+            var numberStrings = input.Split(',');
+
+            var total = 0;
+            foreach (var number in numberStrings)
+            {
+                total = total + int.Parse(number);
+            }
+            return total;
+        }
+    }
+}
